30ad2433c070b72683579f0c82546c1dcc12dae6
Adrian Mowat <adrian.mowat@arnoldclark.co.uk>
2014-03-28 09:57:32 +0000

    Generated new plugin

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject reloadable "0.1.0-SNAPSHOT"
+  :description "Plugin to generate an empty lein project including Stuart Sierra's component library and associated bolilerplate"
+  :url "https://github.com/mowat27/reloadable"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
---
+++ src/leiningen/reloadable.clj
@@ -0,0 +1,6 @@
+(ns leiningen.reloadable)
+
+(defn reloadable
+  "I don't do a lot."
+  [project & args]
+  (println "Hi!"))
